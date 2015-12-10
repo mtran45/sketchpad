2bbb9fc0f616579e413dd83c8c876de320353b27
mtran45 <mtran45@gmail.com>
2015-12-10 23:05:43 +1100

    Add "painting" and basic styling

--- sketchpad.js
+++ sketchpad.js
@@ -1,10 +1,21 @@
 var squares_per_side = 16;
 
 $(document).ready(function() {
+  generate_grid();
+  var $boxes = $(".box");
+  $boxes.mouseenter(function() {
+    $(this).css("background-color","black");
+  });
+  $boxes.mouseleave(function() {
+    
+  });
+});
+
+function generate_grid() {
   for (var i=0;i<squares_per_side*squares_per_side;i++) {
     add_box();
   }
-});
+}
 
 function add_box() {
   var $box = $("<div>", {class: "box"});
