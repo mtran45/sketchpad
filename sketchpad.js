fbadc1d604e0a1baa415164c56716ed2ab42c65e
mtran45 <mtran45@gmail.com>
2015-12-10 23:38:36 +1100

    Add ability to change grid dimensions

--- sketchpad.js
+++ sketchpad.js
@@ -1,25 +1,44 @@
-var squares_per_side = 16;
+var SQUARES_DEFAULT = 16;
 
 $(document).ready(function() {
-  generate_grid();
-  var $boxes = $(".box");
-  $boxes.mouseenter(function() {
-    $(this).addClass("painted");
-  });
-  $("button#clear").click(function() {
-    $boxes.removeClass("painted");
-  });
+  generate_grid(SQUARES_DEFAULT);
+  add_paint_behavior();
+  $("button#clear").click(clear_grid);
+  $("button#size").click(change_grid);
 });
 
-function generate_grid() {
-  for (var i=0;i<squares_per_side*squares_per_side;i++) {
-    add_box();
+function generate_grid(squares) {
+  for (var i=0;i<squares*squares;i++) {
+    add_box(squares);
   }
 }
 
-function add_box() {
+function add_paint_behavior() {
+  $(".box").mouseenter(paint);
+}
+
+function add_box(squares) {
   var $box = $("<div>", {class: "box"});
-  var width = 960/squares_per_side;
+  var width = 960/squares;
   $box.width(width).height(width);
   $('#sketch-container').append($box);
+}
+
+function paint() {
+  $(this).addClass("painted");
+}
+
+function clear_grid() {
+  $(".box").removeClass("painted");
+}
+
+// Change grid dimensons
+function change_grid() {
+  var squares = prompt("How many squares per side?", "16");
+  $(".box").remove();
+  if (squares === null || squares.trim() === "") {
+    squares = SQUARES_DEFAULT;
+  }
+  generate_grid(squares);
+  add_paint_behavior();
 }
